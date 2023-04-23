fixed bugs for vanilla policy gradients

src/MLP.py
@@ -14,16 +14,23 @@ class MLP(nn.Module):
         :param nodes: list containing integers corresponding to our numbers of nodes in each layer
         """
         super(MLP, self).__init__()
+
+        self.s = 0.5
+
         self.nodes = nodes
         self.h_nodes = nodes[1:-1]  # all the hidden nodes
         # initialize the weights for all layers, with std = 1/nodes[i]**0.5
-        self.weights = nn.ParameterList([nn.Parameter(t.randn(nodes[i], nodes[i+1])/(nodes[i]**0.5), requires_grad=True)
+        self.weights = nn.ParameterList([nn.Parameter(self.s * t.randn(nodes[i], nodes[i+1])/(nodes[i]**0.5), requires_grad=True)
                         for i in range(0, len(nodes)-1)])
         # initialize the biases to 0
         self.biases = nn.ParameterList([nn.Parameter(t.zeros(nodes[i+1]), requires_grad=True)
                        for i in range(0, len(nodes)-1)])
         # list containing our transition functions, all ReLU instead of the last one, which is just the identity
         self.sigmas = [F.relu for _ in range(0, len(self.weights)-1)] + [lambda x: x]
+
+        # this arcane value for beta is to set the value of final_sigma(0) at 1, which for beta distributions is
+        # a uniform distribution
+        self.final_sigma = nn.Softplus(beta=np.log(2))
 
     def forward(self, inputs):
         """
@@ -36,7 +43,7 @@ class MLP(nn.Module):
         for w, b, sigma in zip(self.weights, self.biases, self.sigmas):
             x = sigma(x @ w + b)
 
-        return x
+        return self.final_sigma(x)
 
     def set_output_std(self, inputs, out_std):
         """

src/ReplayBuffer.py
@@ -8,8 +8,6 @@ class ReplayBuffer:
     """
     class implementing a replay buffer, keeping track of importance sampling ratios
 
-    todo: we need the ability to add trajectories from vectorized environments
-    todo: compute mean and variance of states in buffer
     todo: need to gracefully treat trajectories with different lengths
 
     """
@@ -23,32 +21,73 @@ class ReplayBuffer:
         self.buffer += trajs
 
     def sample(self, n_traj):
-        # returns a list of trajectories, mean rewards and sampling_log_probs to be passed to the training loop
+        """
+        returns a list of trajectories, mean rewards and sampling_log_probs to be passed to the training loop
+        """
 
         sample_indices = np.random.choice(len(self.buffer), n_traj) \
             if n_traj < len(self.buffer) else np.arange(len(self.buffer))
 
-        obs = [t.tensor(self.buffer[index]['obs']).float() for index in sample_indices]
-        actions = [t.tensor(self.buffer[index]['a']).float() for index in sample_indices]
-        mean_rewards = [t.tensor(self.buffer[index]['rewards']).float().mean() for index in sample_indices]
+        obs = [t.tensor(self.buffer[index]['obs'], dtype=t.float32) for index in sample_indices]
+        actions = [t.tensor(self.buffer[index]['a'], dtype=t.float32)for index in sample_indices]
+        mean_rewards = [t.tensor(self.buffer[index]['r'], dtype=t.float32).mean() for index in sample_indices]
         log_probs = [self.buffer[index]['sampling_log_prob'] for index in sample_indices]
 
         return obs, actions, mean_rewards, log_probs, sample_indices
 
     def remove_indices_from_buffer(self, indices):
-
-        for x in indices.sort()[::-1]:
+        """
+        removes the given indices from the buffer
+        """
+        for x in sorted(indices)[::-1]:
             self.buffer.pop(x)
 
+    def update_IS_ratios(self, IS_ratios, sample_indices):
+        """
+        updates the ['importance_ratio'] field of trajectories at given sample_indices
+        """
+        for index, ratio in zip(sample_indices, IS_ratios):
+            self.buffer[index]['importance_ratio'] = ratio
+
     def purge_low_IS_ratios(self, threshold=1e-7):
-        raise NotImplemented
+        """
+        removes from list all indices with IS_ratios below the threshold
+        """
+        indices_to_remove = []
+        for i in range(len(self.buffer)):
+            if 'importance_ratio' in self.buffer[i]:
+                if self.buffer[i]['importance_ratio'] < threshold:
+                    indices_to_remove.append(i)
+
+        self.remove_indices_from_buffer(indices_to_remove)
 
     def get_data_mean_std(self):
         """
         return the mean and variance over the states currently in the buffer
         """
-        raise NotImplemented
+        state_mean = 0
+        for traj in self.buffer:
+            state_mean += traj['obs'].mean(axis=0)
+
+        state_mean /= len(self.buffer)
+
+        state_var = 0
+        for traj in self.buffer:
+            state_var += ((traj['obs']-state_mean)**2).mean(axis=0)
+
+        state_std = (state_var/len(self.buffer))**0.5
+
+        return state_mean, state_std
 
     def get_average_return(self):
-        raise NotImplemented
+        """
+        computes the average mean_rewards of trajectories in the dataset
+        """
+        total_return = 0
+        for traj in self.buffer:
+            total_return += traj['r'].sum()
+
+        total_return /= len(self.buffer)
+
+        return total_return
 

src/agents/PolicyGradientAgent.py
@@ -16,6 +16,8 @@ class PolicyGradientAgent:
     def __init__(self, env):
 
         # get lower and upper bounds from env, and store them for beta computation
+        self.action_high_bound = t.tensor(env.single_action_space.high)
+        self.action_low_bound = t.tensor(env.single_action_space.low)
 
         # instantiate the model as MLP
         self.state_shape = env.single_observation_space.shape[0]
@@ -25,14 +27,17 @@ class PolicyGradientAgent:
         # instantiates the normalizer module
         self.state_mean = t.zeros(self.state_shape)
         self.state_std = t.ones(self.state_shape)
-
+        self.average_return = 0
         # instantiate the optimizer
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=1e-6, momentum=0.9)
 
     def sample(self, obs):
+        """
+        here obs is a numpy array
+        """
 
         # convert obs to torch tensor
-        obs_t = t.from_numpy(obs)
+        obs_t = t.from_numpy(obs).float()
         if len(obs_t.size()) != 2:
             obs_t = obs_t.unsqueeze(0)
 
@@ -42,43 +47,68 @@ class PolicyGradientAgent:
         # pass it through the network to get beta distribution parameters
         model_output = self.model(obs_t).detach()
         alphas = model_output[:, :self.action_shape]
-        betas = model_output[:, -(self.action_shape+1):]
+        betas = model_output[:, -self.action_shape:]
+
+        # print(model_output.mean(0))
 
         # sample from beta distribution with those parameters
         dist = Beta(alphas, betas)
 
-        return dist.sample()
+        new_sample = dist.sample()
+        # print(new_sample.size())
+
+        log_prob = dist.log_prob(new_sample).sum(dim=1)
+
+        return self.action_low_bound + (self.action_high_bound-self.action_low_bound) * new_sample.numpy(), log_prob.numpy()
 
     def gradient_step(self, trajs, only_return_grad=False):
+        """
+        assume trajs are already stored on gpu and in pytorch form
+        """
 
-        # assume trajs are already stored on gpu and in pytorch form
+        loss = 0
 
-        # pass all observations through normalizer
+        # the log of
+        log_IS_ratios = []
+        obs_, actions_, mean_rewards_, sampling_log_probs_, sample_indices_ = trajs
 
-        # pass trajectories through network, getting the beta parameters for all actions
+        for obs, actions, mean_rewards, sampling_log_probs, sample_indices in \
+                zip(obs_, actions_, mean_rewards_, sampling_log_probs_, sample_indices_):
 
-        # compute log of beta distribution for all trajectories
+            # pass all observations through normalizer obs has shape [max_traj_length, obs_space]
+            obs = (obs-self.state_mean)/self.state_std
 
-        # compute importance sampling ratios and normalize them
+            # pass trajectories through network, getting the beta parameters for all actions
+            model_output = self.model(obs)
+            alphas = model_output[:, :self.action_shape]
+            betas = model_output[:, -(self.action_shape):]
+            dist = Beta(alphas, betas)
+            # compute log of beta distribution for all trajectories
+            # remembering that actions are stored between self.action_low_bound and self.action_high_bound
+            log_probs = dist.log_prob((actions-self.action_low_bound)/(self.action_high_bound-self.action_low_bound))
 
-        # subtract average return from returns in batch
+            IS_ratio = t.exp(log_probs.detach().sum() - sampling_log_probs)
+            log_IS_ratios.append(IS_ratio)
 
-        # define the loss as the sum over trajectories of the log probability scaled by the importance
+            loss += - (mean_rewards - self.average_return) * IS_ratio * log_probs.mean()
 
-        # differentiate that loss
+        loss /= sum(log_IS_ratios)
 
-        # take optim step
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
-        # if only want to return grad, do that instead of taking an optim step
+        # todo: if only want to return grad, do that instead of taking an optim step
 
-        raise NotImplemented
+        return log_IS_ratios
+
 
     def update_input_mean_std(self, mean, std):
         """
         update the mean and standard deviation of the distribution of inputs we expect
         """
-        self.state_mean = mean
-        self.state_std = std
+        self.state_mean = t.tensor(mean).float()
+        self.state_std = t.tensor(std).float()
 
     def update_average_return(self, average_return):
         """

src/utils.py
@@ -3,6 +3,7 @@ import imageio
 
 def get_video_from_env(env, actions, save_path=None):
     """
+    todo: better to sample actions from our agent
     Get a video from the environment using the given actions.
 
     :param env: The environment to get the video from.
@@ -22,33 +23,64 @@ def get_video_from_env(env, actions, save_path=None):
 
     return video
 
+def split_vectorized_traj(trajs):
+
+    new_trajs = []
+
+    for traj in trajs:
+
+        obs = traj['obs']
+        actions = traj['a']
+        rew = traj['r']
+        log_probs = traj['sampling_log_prob']
+
+        n_env = obs.shape[1]
+
+        for i in range(n_env):
+            new_trajs.append({'obs': obs[:, i],
+                              'a': actions[:, i],
+                              'r': rew[:, i],
+                              'sampling_log_prob': log_probs[i]})
+
+    return new_trajs
+
 def sample_trajectories(agent, env, n_traj=1000, max_steps=1000, random_sampling=False):
     """
     Sample trajectories from the environment using the model.
-    todo: package trajectories from vectorized envs as python lists instead
 
     :param model: The model to use to sample the trajectories.
     :param env: The environment to sample the trajectories from, assuming vectorized.
-    :return: A list of trajectories. Each trajectory is a list of dictionaries with keys 'obs', 'a', 'r'.
+    :return: A list of trajectories. Each trajectory is a list of dictionaries with keys 'obs', 'a', 'r', 'sampling_log_prob'.
+
+    traj['obs']: np_array of [max_steps, n_env, obs_shape]
+    traj['a']: np_array of [max_steps, n_env, action_space_shape]
+    traj['r']: np_array of [max_steps, n_env]
+    traj['sampling_log_prob']: np_array of [n_env]
     """
     trajectories = []
     obs_traj = []
     a_traj = []
     r_traj = []
 
-    for traj in range(n_traj):
+    n_iter = int(n_traj/env.num_envs) + 1
+
+    for traj in range(n_iter):
+
         obs, info = env.reset()
 
         traj_log_prob = 0
-
         for step in range(max_steps):
+
+            # if step%10==0:
+            #     print(f"traj{traj}/{n_iter} --- {step}/{max_steps}")
 
             if not random_sampling:
                 action, action_log_prob = agent.sample(obs)  # agent policy that uses the observation and info
+                action = action.numpy()
             else:
                 action = env.action_space.sample()
-                # this is uniform sampling
-                action_log_prob = -np.log(env.action_space.high - env.action_space.low)
+                # this is uniform sampling, we're treating all action spaces as between 0 and 1
+                action_log_prob = np.zeros(env.num_envs)
 
             observation, reward, terminated, truncated, info = env.step(action)
             obs_traj.append(observation)
@@ -56,7 +88,7 @@ def sample_trajectories(agent, env, n_traj=1000, max_steps=1000, random_sampling
             r_traj.append(reward)
             traj_log_prob += action_log_prob
 
-            if terminated or truncated or step == max_steps-1:
+            if terminated.any() or truncated.any() or step == max_steps-1:
                 trajectories.append({'obs': np.stack(obs_traj, axis=0),
                                      'a': np.stack(a_traj, axis=0),
                                      'r': np.stack(r_traj, axis=0),
@@ -66,5 +98,19 @@ def sample_trajectories(agent, env, n_traj=1000, max_steps=1000, random_sampling
                 r_traj = []
                 break
 
-    return trajectories
+    return split_vectorized_traj(trajectories)
 
+if __name__ == "__main__":
+
+    from agents.PolicyGradientAgent import *
+    import gymnasium as gym
+
+    env_name = "HalfCheetah-v4"
+    num_envs = 32
+    env = gym.vector.make(env_name, num_envs=num_envs, asynchronous=True)
+    agent = PolicyGradientAgent(env)
+
+    trajs = sample_trajectories(agent, env, 20, max_steps=100, random_sampling=False)
+
+    # obs, info = env.reset()
+    # action, action_log_prob = agent.sample(obs)